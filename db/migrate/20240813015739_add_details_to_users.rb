13feeb25087ee8805a7e1d38d9a9c7c74e76ffb1
Mia OConnell <miaxoconnell@gmail.com>
2024-08-13 11:10:21 +0900

    User model migration done

---
+++ db/migrate/20240813015739_add_details_to_users.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :description, :text
+    add_column :users, :location, :string
+  end
+end
