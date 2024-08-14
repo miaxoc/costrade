84f11edbd16004af65748236da83463cf96613a3
Mia OConnell <miaxoconnell@gmail.com>
2024-08-14 20:56:32 +0900

    css

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -6,9 +6,16 @@ class CostumesController < ApplicationController
   end
 
   def new
+    @costume = Costume.new
   end
 
   def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      redirect_to costume_path(@costume)
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def show
