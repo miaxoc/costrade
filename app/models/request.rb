0c9e70ff8c3881e8fd5005182c06e1e4eca6e5e6
Max McCain <itsmaximuk@gmail.com>
2024-08-14 10:35:46 +0900

    saving changes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :costume
   belongs_to :user
-
+  ## had to disable this... not sure why
   # validates :status, default: nil
   validates :start_date, presence: true #js
   validates :end_date, presence: true #js
