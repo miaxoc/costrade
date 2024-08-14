8c5cd84a8c9f1982bec2346561c8601b77f5763d
Yu Bando Foster <yu.bando.90@gmail.com>
2024-08-14 14:19:33 +0900

    WIP

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,5 +1,5 @@
 class CostumesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @costumes = Costume.all
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # comment
+    @costumes = Costume.limit(9)
   end
 end
