4951709045bc633fbb351a665e742db22894272b
Yu Bando Foster <yu.bando.90@gmail.com>
2024-08-15 16:31:05 +0900

    WIP

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,11 +1,12 @@
 class Request < ApplicationRecord
   belongs_to :costume
   belongs_to :user
+  has_many :reviews
   ## had to disable this... not sure why
   # validates :status, default: nil
   validates :start_date, presence: true #js
   validates :end_date, presence: true #js
 
-  enum :status, [:pending, :accepted, :declined]
+  enum :status, [:pending, :accepted, :declined, :completed]
   # enum :status, {pending: "pending", accepted: "accepted", declined: "declined"}
 end
